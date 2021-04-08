dc3211156a9f1cd0c3eaf039fd6168a375bb4faa
Marcelo Savian <marcelo.savian@gmail.com>
2021-04-08 01:35:44 -0300

    feat: first peage login

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import { userModule } from "./user.module";
+import { alert } from "./alert.module";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    alert,
+    userModule,
+  },
 });
